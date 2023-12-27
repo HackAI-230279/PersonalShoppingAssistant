98121916d82a8772efe3c33a1a986cd7f96636cc
Shashwat Verma <68842852+Imyr@users.noreply.github.com>
2023-12-27 20:49:22 +0530

    Amazon added and full CLI implementation

--- client.py
+++ client.py
@@ -1,14 +1,13 @@
+from uagents import Agent, Context, Model
 from dotenv import load_dotenv
+from typing import Dict, List
+import logging
 
 load_dotenv()
 
-from typing import Dict, List
-
-from uagents import Agent, Context, Model
-
-
 class Message(Model):
     message: str
+
 
 class Query(Model):
     text: str
@@ -36,34 +35,41 @@ client_agent = Agent(
     endpoint=["http://127.0.0.1:8001/submit"],
 )
 
+
 @client_agent.on_event("startup")
 async def start(ctx: Context):
-    # ctx.logger.info(f"Hello, my name is {ctx.name} and address is {ctx.address}")
+    logging.setLevel = logging.WARN
+    ctx.logger.info(f"Hello, my name is {ctx.name} and address is {ctx.address}")
     await ctx.send(
         "agent1qw7gh69x7q2lj23mrc3n24yt7qjfe6jaec863907geqpz3mx47q06zg7rnr",
-        Query(
-            text=input("\nPlease enter product name: ")
-        )
+        Query(text=input("\nPlease enter product name: ")),
     )
+
 
 @client_agent.on_event("shutdown")
 async def stop(ctx: Context):
-    print("Thanks for shopping!")
+    print("\nThanks for shopping!")
+
 
 @client_agent.on_message(model=Results)
 async def search(ctx: Context, sender: str, msg: Results):
-    print(msg.text)
-    await ctx.send(
-        sender,
-        message=Choice(
-            choice=input("\nEnter choice: "),
-            results=msg.results,
+    if len(msg.results) != 0:
+        print("\n" + msg.text)
+        await ctx.send(
+            sender,
+            message=Choice(
+                choice=input("\nEnter choice: "),
+                results=msg.results,
+            ),
         )
-    )
+    else:
+        print("\nSorry! No products seem to be available for your selected query.")
+
 
 @client_agent.on_message(model=Product)
 async def search(ctx: Context, sender: str, msg: Product):
     print(msg.text)
+
 
 from uagents.setup import fund_agent_if_low
 
--- src/backend/sites/__main__.py
+++ src/backend/sites/__main__.py
@@ -1,6 +1,42 @@
 import requests
 from bs4 import BeautifulSoup
 from typing import List
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from amazondata.search_result_extractor import SearchResultExtractor
+
+def amazon(query: str) -> List:
+    service = Service(executable_path=r'C:\Users\shash\Downloads\chromedriver-win64\chromedriver.exe')
+    options = webdriver.ChromeOptions()
+    options.add_argument('--headless')
+    driver = webdriver.Chrome(service=service, options=options)
+
+    url = f'https://www.amazon.in/s?k={query}&ref=nb_sb_noss_2&sr_pg_1'
+
+    try:
+        driver.get(url)
+        html_code = driver.page_source
+    finally:
+        driver.quit()
+
+    search_result_extractor = SearchResultExtractor()
+    
+    l = []
+    for val in search_result_extractor.extract_search_results(html_code)["products"]:
+        try:
+            skip = 0
+            for i in query.split(" "):
+                if i not in val["title"].lower():
+                    skip = 1
+                    break
+            if skip:
+                continue        
+            l.append({"name": val["title"], "price": int(val["price"].replace("₹", "").replace(",", "")), "link": val["url"].split("?")[0], "site": "Amazon"})
+        except:
+            pass
+    
+    return l
+    
 
 def flipkart(query: str) -> List:
     source = requests.get(f"https://www.flipkart.com/search?q={query}")
@@ -45,6 +81,7 @@ def flipkart(query: str) -> List:
 def sorted_results(query: str) -> List:
     res = []
     res += flipkart(query.replace(" ", "+"))[:5]
-    
+    res += amazon(query)[:5]
+
     res = sorted(res, key=lambda x: x["price"])
     return "Here you go!\n"+"\n".join([f"{idx+1}: {val['name'][:70]}... ({val['site']}, Rs. {val['price']})" for idx, val in enumerate(res)]), res
